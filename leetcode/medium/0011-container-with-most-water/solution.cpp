04f9f348b60fa768965f2cf2babb729c917345aa
moon1426 <z.madhura14@gmail.com>
2026-07-10 20:15:19 +0530

    ✅ Container With Most Water | Medium | C++ | Runtime: 0 ms (100.00%) | Memory: 63 MB | 2026-07-11

---
+++ leetcode/medium/0011-container-with-most-water/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left =0;
+        int right=height.size()-1;
+        int maxwater=0;
+        while(left<right)
+        {
+            int width = right-left;
+            int currentheight=min(height[left],height[right]);
+            int currentwater=width*currentheight;
+            maxwater=max(maxwater,currentwater);
+            //
+            if (height[left]<height[right])
+            {
+                left++;
+            }
+            else{
+                right--;
+            }
+
+            }
+            return maxwater;
+        }
+        
+    
+};
